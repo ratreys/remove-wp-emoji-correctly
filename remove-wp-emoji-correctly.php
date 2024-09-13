a8d5c21c4ceca412d21e460452b8c12bfa331e38
Rey Ortilla <rsortilla@gmail.com>
2024-09-14 00:03:19 +0800

    Update remove-wp-emoji-correctly.php

--- remove-wp-emoji-correctly.php
+++ remove-wp-emoji-correctly.php
@@ -16,11 +16,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * We can find the default filters here:
+ * Default filters are found at wp-includes/default-filters.php
  * 
- * @link wp-includes/default-filters.php
- * 
- * We no longer need to manually remove emoji_svg_url prefetch from wp_resource_hints since it's no longer included by default.
+ * We no longer need to manually remove emoji_svg_url prefetch from wp_resource_hints since it's no longer
+ * included by default.
  * 
  * @link https://core.trac.wordpress.org/changeset/53904
  */
@@ -44,7 +43,7 @@ final class Remove_WPEmoji_Correctly {
         /** Actions. */
         remove_action( 'wp_head',            'print_emoji_detection_script', 7 );
         remove_action( 'wp_enqueue_scripts', 'wp_enqueue_emoji_styles' );
-        remove_action( 'wp_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility. Unhooked by wp_enqueue_emoji_styles.
+        remove_action( 'wp_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility.
 
         /** Prevent conversion of emoji to a static img element. */
         remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
@@ -60,7 +59,7 @@ final class Remove_WPEmoji_Correctly {
         /** Actions. */
         remove_action( 'admin_print_scripts',   'print_emoji_detection_script' );
         remove_action( 'admin_enqueue_scripts', 'wp_enqueue_emoji_styles' );
-        remove_action( 'admin_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility. Unhooked by wp_enqueue_emoji_styles.
+        remove_action( 'admin_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility.
 
         /** For those using classic editor. */
         add_filter( 'tiny_mce_plugins', array( __CLASS__, 'remove_wpemoji_plugin' ) );
@@ -77,7 +76,7 @@ final class Remove_WPEmoji_Correctly {
      *      'wordpress',     'wpgallery',    'wpdialogs',   'tabfocus',    'charmap',    'wpemoji',
      *      'wplink',        'wpview',       'lists',       'media',       'paste',      'hr',
      * 
-     * @link wp-includes/class-wp-editor.php starting line 421
+     * Please see line 421 of wp-includes/class-wp-editor.php
      * 
     */
     public static function remove_wpemoji_plugin( $default_tiny_mce_plugins ) {
