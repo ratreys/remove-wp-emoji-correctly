20276c660ff8f09606cdea72e0cc8f3ebae2e3b7
Rey Ortilla <rsortilla@gmail.com>
2024-09-13 23:10:11 +0800

    Update remove-wp-emoji-correctly.php

--- remove-wp-emoji-correctly.php
+++ remove-wp-emoji-correctly.php
@@ -41,47 +41,46 @@ final class Remove_WPEmoji_Correctly {
     /** Private constructor to prevent direct instantiation. */
     private function __construct() {/** There is nothing here. */}
 
-    public function init() {
+    public static function init() {
         /** Actions. */
-        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'wp_head',            'print_emoji_detection_script', 7 );
         remove_action( 'wp_enqueue_scripts', 'wp_enqueue_emoji_styles' );
-        // Retained for backwards compatibility. Unhooked by wp_enqueue_emoji_styles.
-        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_action( 'wp_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility. Unhooked by wp_enqueue_emoji_styles.
 
         /** Prevent conversion of emoji to a static img element. */
         remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
         remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+        remove_filter( 'wp_mail',          'wp_staticize_emoji_for_email' );
 
         /** Embeds. */
-        remove_action( 'embed_head', 'print_emoji_detection_script' );
+        remove_action( 'embed_head',            'print_emoji_detection_script' );
         remove_action( 'enqueue_embed_scripts', 'wp_enqueue_emoji_styles' );
     }
 
-    public function admin_init() {
+    public static function admin_init() {
         /** Actions. */
-        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'admin_print_scripts',   'print_emoji_detection_script' );
         remove_action( 'admin_enqueue_scripts', 'wp_enqueue_emoji_styles' );
-        // Retained for backwards compatibility. Unhooked by wp_enqueue_emoji_styles.
-        remove_action( 'admin_print_styles', 'print_emoji_styles' );
+        remove_action( 'admin_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility. Unhooked by wp_enqueue_emoji_styles.
 
         /** For those using classic editor. */
-        add_filter( 'tiny_mce_plugins', [ $this, 'remove_wpemoji_plugin' ] );
+        add_filter( 'tiny_mce_plugins', array( __CLASS__, 'remove_wpemoji_plugin' ) );
     }
 
     /**
      * Filter out 'wpemoji' inside TinyMCE editor.
      * 
-     * @param   array   $default_tiny_mce_plugins {
-     *          List of default TinyMCE plugins:
-     *              'charmap', 'colorpicker', 'hr', 'lists', 'media', 'paste', 'tabfocus',
-     *              'textcolor', 'fullscreen', 'wordpress', 'wpautoresize', 'wpeditimage'
-     *              'wpemoji', 'wpgallery', 'wplink', 'wpdialogs', 'wptextpattern', 'wpview'
-     *          }
+     * @param array $default_tiny_mce_plugins {
+     *      List of default TinyMCE plugins:
+     * 
+     *      'charmap', 'colorpicker', 'hr', 'lists', 'media', 'paste', 'tabfocus',
+     *      'textcolor', 'fullscreen', 'wordpress', 'wpautoresize', 'wpeditimage'
+     *      'wpemoji', 'wpgallery', 'wplink', 'wpdialogs', 'wptextpattern', 'wpview'
+     * }
     */
-    public function remove_wpemoji_plugin( $default_tiny_mce_plugins ) {
+    public static function remove_wpemoji_plugin( $default_tiny_mce_plugins ) {
         if ( is_array( $default_tiny_mce_plugins ) && in_array( 'wpemoji', $default_tiny_mce_plugins, true ) ) {
-            return array_diff( $default_tiny_mce_plugins, [ 'wpemoji' ] );
+            return array_diff( $default_tiny_mce_plugins, array('wpemoji') );
         }
 
         return $default_tiny_mce_plugins;
@@ -89,9 +88,9 @@ final class Remove_WPEmoji_Correctly {
 }
 
 if ( is_admin() ) {
-    add_action( 'admin_init', [ Remove_WPEmoji_Correctly::instance(), 'admin_init' ] );
+    add_action( 'admin_init', array( 'Remove_WPEmoji_Correctly', 'admin_init' ) );
 }  else {
-    add_action( 'init', [ Remove_WPEmoji_Correctly::instance(), 'init' ] );
+    add_action( 'init', array( 'Remove_WPEmoji_Correctly', 'init' ) );
 }
 
 endif;
