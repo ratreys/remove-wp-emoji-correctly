a5e2b09c3dd9e666b587e0b9356a7550ac9de6df
Rey Ortilla <rsortilla@gmail.com>
2024-09-20 21:45:53 +0800

    Update remove-wp-emoji-correctly.php

--- remove-wp-emoji-correctly.php
+++ remove-wp-emoji-correctly.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Remove WP Emoji — Correctly
+ * Plugin Name:       Remove WP Emoji Correctly
  * Plugin URI:        https://selftawt.com/disable-wpemoji-correctly/
  * Description:       The right way to remove or disable WordPress emoji added in v4.2. Make your header clean, lean, and mean.
  * Version:           1.0.0
@@ -24,6 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * included by default.
  * 
  * @link https://core.trac.wordpress.org/changeset/53904
+ * 
  */
 if ( ! class_exists( 'Remove_WP_Emoji_Correctly' ) ) :
 final class Remove_WP_Emoji_Correctly {
@@ -44,8 +45,8 @@ final class Remove_WP_Emoji_Correctly {
     public static function init() {
         /** Actions. */
         remove_action( 'wp_head',            'print_emoji_detection_script', 7 );
-        remove_action( 'wp_enqueue_scripts', 'wp_enqueue_emoji_styles' );
         remove_action( 'wp_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility.
+        remove_action( 'wp_enqueue_scripts', 'wp_enqueue_emoji_styles' );
 
         /** Prevent conversion of emoji to a static img element. */
         remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
@@ -60,15 +61,16 @@ final class Remove_WP_Emoji_Correctly {
     public static function admin_init() {
         /** Actions. */
         remove_action( 'admin_print_scripts',   'print_emoji_detection_script' );
-        remove_action( 'admin_enqueue_scripts', 'wp_enqueue_emoji_styles' );
         remove_action( 'admin_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility.
+        remove_action( 'admin_enqueue_scripts', 'wp_enqueue_emoji_styles' );
 
         /** For those using classic editor. */
         add_filter( 'tiny_mce_plugins', array( __CLASS__, 'remove_wpemoji_plugin' ) );
     }
 
     /**
-     * Filter out 'wpemoji' inside TinyMCE editor.
+     * Remove the default 'wpemoji' plugin inside TinyMCE editor.
+     * Please see line 421 of wp-includes/class-wp-editor.php
      * 
      * @param array $default_tiny_mce_plugins
      * 
@@ -77,8 +79,6 @@ final class Remove_WP_Emoji_Correctly {
      *      'wptextpattern', 'wpautoresize', 'colorpicker', 'wpeditimage', 'fullscreen', 'textcolor',
      *      'wordpress',     'wpgallery',    'wpdialogs',   'tabfocus',    'charmap',    'wpemoji',
      *      'wplink',        'wpview',       'lists',       'media',       'paste',      'hr',
-     * 
-     * Please see line 421 of wp-includes/class-wp-editor.php
      * 
     */
     public static function remove_wpemoji_plugin( $default_tiny_mce_plugins ) {
