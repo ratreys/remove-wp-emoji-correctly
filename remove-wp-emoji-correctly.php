7c2cef460daab83c94fa46c48444ddc1d011ba71
Rey Ortilla <rsortilla@gmail.com>
2024-10-07 17:50:12 +0800

    Update remove-wp-emoji-correctly.php

--- remove-wp-emoji-correctly.php
+++ remove-wp-emoji-correctly.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Remove WP Emojis — Correctly
  * Plugin URI:        https://selftawt.com/disable-wpemoji-correctly
  * Description:       The right way to remove or disable emoji support that was added in WordPress v4.2. Make your header clean, lean, and mean.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.4
  * Requires PHP:      7.4
  * Author:            Rey Sanchez
