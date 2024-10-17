f157b40c0e5906207e0a7787d3e03ce6be6d3802
Rey Ortilla <rsortilla@gmail.com>
2024-10-17 21:11:46 +0800

    Update main plugin file

--- remove-wp-emoji-correctly.php
+++ remove-wp-emoji-correctly.php
@@ -3,15 +3,15 @@
  * Plugin Name:       Remove WP Emojis — Correctly
  * Plugin URI:        https://selftawt.com/disable-wpemoji-correctly
  * Description:       The right way to remove or disable emoji support that was added in WordPress v4.2. Make your header clean, lean, and mean.
- * Version:           1.0.1
- * Requires at least: 6.4
+ * Version:           1.1.0
+ * Requires at least: 6.5
  * Requires PHP:      7.4
  * Author:            Rey Sanchez
  * Author URI:        https://selftawt.com
  * License:           GPL-3.0
  */
 
-namespace Selftawt\RWPEC;
+namespace Selftawt\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
@@ -20,7 +20,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Default filters are found at wp-includes/default-filters.php
  * 
+ * @link https://core.trac.wordpress.org/browser/tags/6.5/src/wp-includes/default-filters.php
  * @link https://core.trac.wordpress.org/browser/tags/6.6/src/wp-includes/default-filters.php
+ * @link https://core.trac.wordpress.org/browser/tags/6.7/src/wp-includes/default-filters.php
  * 
  * We no longer need to manually remove emoji_svg_url prefetch from wp_resource_hints since it's no longer
  * included by default.
@@ -28,6 +30,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @link https://core.trac.wordpress.org/changeset/53904
  * 
  */
+if ( ! class_exists( '\Selftawt\Plugin\Remove_WP_Emojis_Correctly' ) ) :
 final class Remove_WP_Emojis_Correctly {
 
     private static $instance = null;
@@ -41,29 +44,29 @@ final class Remove_WP_Emojis_Correctly {
     }
 
     /** Private constructor to prevent direct instantiation. */
-    private function __construct() {/** There is nothing here. */}
+    private function __construct() { /** There is nothing here. */ }
 
     public static function init() {
         /** Actions. */
-        remove_action( 'wp_head',            'print_emoji_detection_script', 7 );
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
         remove_action( 'wp_enqueue_scripts', 'wp_enqueue_emoji_styles' );
-        remove_action( 'wp_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility.
+        remove_action( 'wp_print_styles', 'print_emoji_styles' ); // Retained for backwards compatibility.
 
         /** Prevent conversion of emoji to a static img element. */
         remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
         remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-        remove_filter( 'wp_mail',          'wp_staticize_emoji_for_email' );
+        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 
         /** Embeds. */
-        remove_action( 'embed_head',            'print_emoji_detection_script' );
+        remove_action( 'embed_head', 'print_emoji_detection_script' );
         remove_action( 'enqueue_embed_scripts', 'wp_enqueue_emoji_styles' );
     }
 
     public static function admin_init() {
         /** Actions. */
-        remove_action( 'admin_print_scripts',   'print_emoji_detection_script' );
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
         remove_action( 'admin_enqueue_scripts', 'wp_enqueue_emoji_styles' );
-        remove_action( 'admin_print_styles',    'print_emoji_styles' ); // Retained for backwards compatibility.
+        remove_action( 'admin_print_styles', 'print_emoji_styles' ); // Retained for backwards compatibility.
 
         /** For those using classic editor. */
         add_filter( 'tiny_mce_plugins', array( __CLASS__, 'remove_wpemoji_plugin' ) );
@@ -92,7 +95,9 @@ final class Remove_WP_Emojis_Correctly {
 }
 
 if ( is_admin() ) {
-    add_action( 'admin_init', array( '\Selftawt\RWPEC\Remove_WP_Emojis_Correctly', 'admin_init' ) );
+    add_action( 'admin_init', array( '\Selftawt\Plugin\Remove_WP_Emojis_Correctly', 'admin_init' ) );
 }  else {
-    add_action( 'init', array( '\Selftawt\RWPEC\Remove_WP_Emojis_Correctly', 'init' ) );
+    add_action( 'init', array( '\Selftawt\Plugin\Remove_WP_Emojis_Correctly', 'init' ) );
 }
+
+endif;
